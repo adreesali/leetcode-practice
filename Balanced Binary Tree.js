9b6487740d226b65c9e9e1a132955ed1b8e185e1
adrees-ali <109959486+adreesali@users.noreply.github.com>
2023-09-19 22:53:23 +0500

    Update Balanced Binary Tree.js

--- Balanced Binary Tree.js
+++ Balanced Binary Tree.js
@@ -1,30 +1,3 @@
-var isBalanced = function(root) {
-    // yeh function line is balance ko declare krti hai keh jo aik binary tree ko input laita hai oor check krta hai ke tfree balanced ha ya nhi hai 
-    function height(root) {
-        // function "height" declare karta ha jo ke vales ki height ko nikalta ha
-        if(root == null) return 0
-        // Yeh dekhta hai ke agar di gai root khali (null) hai to uski height zero hoti ha
-        
-        return 1+Math.max(height(root.left), height(root.right));
-        //  yeh element ki height nikalta hai, ismai node ke left aur right subtrees mein se jo zyada height wala hai, woh select kia jata hai aur ek unit (1) add kiya jata hai
-    }
-    if(root == null)
-        return true;
-    // yeh dekhta ha ke agar di gai value khali (null) hai to usko  balanced samjha jata ha
-
-    
-    let leftH = height(root.left);
-    // Yeh  value ke left subtree ki height ko "leftH" mein rakhta ha
-    let rightH = height(root.right);
-    // Yeh  value ke right subtree ki height ko "rightH" mein store krta ha
-    
-    return Math.abs(leftH - rightH) <= 1 && isBalanced(root.left) == true && isBalanced(root.right) == true;
-    //  Yeh dekhta hai ke current root ke left aur right subtrees balanced hain aur unki heights mein zyada se zyada aik unit ka difference ha ya nhi Agar yeh condition puri hoti hai, to current value ko bhi balanced maan liya jata hai
-};
-
-
-
-// 2nd
 
 
 var isBalanced = function(root) {
@@ -44,3 +17,14 @@ var isBalanced = function(root) {
     
     return Math.abs(leftH - rightH) <= 1 && isBalanced(root.left) == true && isBalanced(root.right) == true;
 };
+
+// height function: Yeh function ek binary tree ke height ko calculate karta hai. Agar input root null hota hai, toh iska height 0 hota hai. Agar root null nahi hai, 
+//toh function recursive tarike se left aur right subtrees ke maximum height ko lekar usme 1 add karke return karta hai.
+
+// isBalanced function: Yeh function binary tree ke balance hone ko check karta hai.
+// Sabse pehle, function check karta hai ki agar root null hai toh yeh tree balanced hai, kyun ki null tree ka height 0 hota hai, aur height difference 0 bhi hota hai, toh true return kiya jata hai.
+// Agar root null nahi hai, toh function root ke left aur right subtrees ke heights ko calculate karta hai.
+// Fir, function check karta hai ki left subtree aur right subtree ke heights ka absolute difference 1 se kam hai ya nahi (Math.abs(leftH - rightH) <= 1).
+// Uske baad, function recursive tarike se left subtree aur right subtree ke liye isBalanced function ko call karta hai. Yani, function left subtree aur right subtree ke liye bhi yehi checks lagata hai ki woh balanced hai ya nahi.
+// Agar dono subtrees balanced hai, toh true return kiya jata hai, nahi toh false return kiya jata hai.
+// Overall, yeh code binary tree ke har node ke liye balance hone ko check karta hai. Agar har node ke liye balance check hota hai aur tree bhi balanced hai, toh true return hota hai, nahi toh false return hota hai.
